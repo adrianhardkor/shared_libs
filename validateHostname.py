b73feb6b1a03ccbebd6e9a8cbd98c0b9b92e8ec6
Adrian <adrian@Adrians-MacBook-Pro.local>
2021-01-15 15:47:22 -0600

    manual_push

---
+++ validateHostname.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import os
+import sys
+import wcommon as wc
+import json
+import re
+# wc.jenkins_header(); # load inputs from Jenkinsfile
+# wc.jd(wc.wcheader)
+
+# hostname = 'ARC UAT1 HUH CMR 01'
+# print(hostname)
+
+
+def sanitizeARChostname(hostname):
+	hostname = hostname.replace(' ','').replace('-','')
+	print(hostname)
+	labs = ['ARC']
+	markets = ['UAT1', 'UAT2', 'VDC1', 'MDEV', 'SIT1', 'CLOD', 'BKBN', 'PODS']
+	# services = '%s%s%s'
+	functions = ['BBR', 'EPR', 'VAR', 'DAR', 'DRR', 'CMR', 'MSR', 'AGS', 'SWI', 'BAR', 'OTN', 'CMT', 'VCE', 'FRW', 'TRM', 'L1S', 'TST', 'STS', 'W1S']
+	iterations = '%d%d'
+
+	INVALID = []
+	components = {}
+
+	components['lab'] = hostname[:3].upper()
+	if components['lab'] not in labs: INVALID.append(components['lab'])
+
+	components['market'] = hostname[3:7].upper()
+	if components['market'] not in markets: INVALID.append(components['market'])
+
+	components['service'] = hostname[7:10].upper()
+	if re.search('...', components['service']) is False: INVALID.append(components['service'])
+
+	components['function'] = hostname[10:13].upper()
+	if components['function'] not in functions: INVALID.append(components['function'])
+
+	components['iteration'] = hostname[13:].upper()
+	if re.search('..', components['iteration']) is False: INVALID.append(components['iteration'])
+
+	components['INVALID'] = INVALID
+	print(json.dumps(components, indent=2))
+
+sanitizeARChostname(wc.argv_dict['host'])
+exit(0)
--- velocity.py
+++ velocity.py
@@ -15,33 +15,49 @@ class VELOCITY():
 		self.__name__ = 'VELOCITY'
 	def REST_GET(self, url, params={}):
 		# print('\t' + self.V + url)
-		return(json.loads(wc.REST_GET(self.V + url, user=self.user, pword=self.pword, params=params)))
+		# page = '1'
+		# while page is ''
+		# url = url + '?offset={offset}&limit={limit}&filter={filter}' == MAX 200 then OFFSET
+		url = url + '?limit=200'
+		data = json.loads(wc.REST_GET(self.V + url, user=self.user, pword=self.pword, params=params))
+		return(data)
+	def GetTopologies(self):
+		top = VELOCITY.REST_GET(self, '/topology/v12/topologies')['topologies']
+		out = {}
+		for t in top:
+			out[t['id']] = t
+		return(out)
 	def GetInventory(self):
+		top = VELOCITY.GetTopologies(self)
 		out = {}
 		data = VELOCITY.REST_GET(self, '/inventory/v13/devices', params={'includeProperties':True, 'includePortGroups': True})
 		for device in data['devices']:
-			wc.pairprint(device['id'], device['name'])
 			out[device['id']] = {'name': device['name'], 'portGroups':[]}
+			wc.pairprint(device['name'], out[device['id']])
 			for prop in device['properties']:
 				out[device['id']][prop['name']] = prop['value']
+			wc.pairprint(device['id'], out[device['id']])
 			for pg in device['portGroups']:
 				if pg['id'] != None:
-					out[device['id']]['portGroups'].append(pg)
+					pg['ports'] = {}
 					# wc.pairprint(device['id'], device['name'] + '\t' + str(pg['id']))
 					# wc.jd(VELOCITY.REST_GET(self, '/inventory/v13/device/%s/port_group/%s' % (str(device['id']), str(pg['id']))))
-					for p in VELOCITY.REST_GET(self, '/inventory/v13/device/%s/port_group/%s' % (str(device['id']), str(pg['id'])))['ports']:
-						o = ['  ', p['name'], p['id'], time.ctime(p['linkChecked'])]
+					pp = VELOCITY.REST_GET(self, '/inventory/v13/device/%s/port_group/%s' % (str(device['id']), str(pg['id'])))
+					for p in pp['ports']:
+						pg['ports'][p['name']] = {'id':p['id'],'linkChecked':time.ctime(p['linkChecked'])}
 						if p['isLocked']:
-							o.append(p['lockUtilizationType'])
-							o.append(p['connectedPortParentName'])
-							o.append(p['connectedPortParentId'])
-							o.append(p['connectedPortName'])
-							o.append(p['connectedPortId'])
-						wc.listprint('  ', o)
+							pg['ports'][p['name']]['lockUtilizationType'] = p['lockUtilizationType']
+							pg['ports'][p['name']]['connectedPortParentName'] = p['connectedPortParentName']
+							pg['ports'][p['name']]['connectedPortParentId'] = p['connectedPortParentId']
+							pg['ports'][p['name']]['connectedPortName'] = p['connectedPortName']
+							pg['ports'][p['name']]['connectedPortId'] = p['connectedPortId']
+						pg['ports'][p['name']] = p
+						# wc.pairprint(p['name'], pg['ports'][p['name']])
+					out[device['id']]['portGroups'].append(pg)
 		return(out)
-		# wc.jd(data)
 
 V = VELOCITY(wc.argv_dict['IP'], wc.argv_dict['user'], wc.argv_dict['pass'])
-V.GetInventory()
-# wc.jd(V.REST_GET('/inventory/v13/devices'))
+wc.jd(V.GetInventory())
+wc.jd(V.GetTopologies())
 exit(0)
+
