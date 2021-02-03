eb1b60c631f013e197dc1be9dcd36d92ba3b109e
Adrian <adrian@Adrians-MacBook-Pro.local>
2021-02-03 11:08:09 -0600

    manual_push

--- Stc.py
+++ Stc.py
@@ -19,7 +19,7 @@ def init(project_name):
 		Active='TRUE', \
 		LocalActive='TRUE', \
 		Name='StcSystem 1')
-	wc.pairprint('[INFO] Built System', wc.timer_index_since(system_time))
+	# wc.pairprint('[INFO] Built System', wc.timer_index_since(system_time))
 	project_time = wc.timer_index_start()
 	# wc.jd(stc.get('system1'))
 	Project_1 = stc.create('Project', \
@@ -29,7 +29,7 @@ def init(project_name):
 		Active='TRUE', \
 		LocalActive='TRUE', \
 		Name=project_name)
-	wc.pairprint('[INFO] Built Project: ' + project_name, wc.timer_index_since(project_time))
+	# wc.pairprint('[INFO] Built Project: ' + project_name, wc.timer_index_since(project_time))
 	# wc.jd(stc.get(Project_1))
 	return(system1,Project_1)
 
@@ -38,7 +38,7 @@ def getChassisList():
 	return(stc.get(hMgr,'children-PhysicalChassis').split())
 
 def disconnectChassis():
-	print('[INFO] DISCONNECTING FROM ALL STC CHASSIS')
+	# print('[INFO] DISCONNECTING FROM ALL STC CHASSIS')
 	stc.perform("ChassisDisconnectAll")
 
 def connectChassis(ip):
@@ -47,7 +47,7 @@ def connectChassis(ip):
 	result = stc.connect(ip)
 	stc.apply()
 	hPhysical = stc.create('PhysicalChassisManager', under='system1')
-	wc.pairprint('[INFO] Connect to CHASSIS: ' + ip, wc.timer_index_since(connect_time))
+	# wc.pairprint('[INFO] Connect to CHASSIS: ' + ip, wc.timer_index_since(connect_time))
 	# wc.jd(stc.get(hPhysical))
 	return(result)
 
@@ -67,7 +67,7 @@ def port_config(hProject, portname):
 		Active='TRUE', \
 		LocalActive='TRUE', \
 		Name="Port @ ' + portname")
-	wc.pairprint('[INFO] Built Port: ' + portname, wc.timer_index_since(portbuild_time))
+	# wc.pairprint('[INFO] Built Port: ' + portname, wc.timer_index_since(portbuild_time))
 	return(Port_1)
 
 def getConnectedChassisPhysical(szChassisIpList):
