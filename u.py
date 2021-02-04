1e5ce352b2f15908a29fa1fbaaffc20c8f3949aa
Adrian <adrian@Adrians-MacBook-Pro.local>
2021-02-04 16:53:17 -0600

    manual_push

--- u.py
+++ u.py
@@ -6,7 +6,8 @@ import json
 import re
 # wc.jenkins_header(); # load inputs from Jenkinsfile
 # wc.jd(wc.wcheader)
-
+import time
+print(int(time.time()))
 # hostname = 'ARC UAT1 HUH CMR 01'
 # print(hostname)
 
