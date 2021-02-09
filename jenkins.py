e23503428492a7e947eb2c1da121b7954c9c49ef
Adrian <adrian@Adrians-MacBook-Pro.local>
2021-02-09 15:46:59 -0600

    manual_push

--- jenkins.py
+++ jenkins.py
@@ -56,7 +56,7 @@ class JENKINS():
 			parameters_url.append(p + '=' + parameters[p])
 		wc.jd(self.REST_POST('/job/%s/buildWithParameters%s' % (PipelineName, '?' + '&'.join(parameters_url)), Parameters))
 		time.sleep(2)
-		wc.jd(self.REST_GET('/jenkins/api/json?tree=jobs[displayName,lastBuild[number]]&xpath=/hudson/job&includes=/hudson/job/displayName[contains(.,%27myjobnamefilter%27)]&wrapper=job_names&pretty=true'))
+		print(self.REST_GET('http://10.88.48.21:8080/job/ARC2/lastCompletedBuild/console'))
 
 J = JENKINS(wc.argv_dict['IP'], wc.argv_dict['user'], wc.argv_dict['token'])
 J.RunPipeline('ARC2', {'Playbook':'ARC_GetFactsMultivendor','sendmail':'jenkinsAuto'})
