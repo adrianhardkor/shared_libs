c14f9084ce48ac8593bc1e0d8df3c6d5946cc1dc
Adrian <adrian@Adrians-MacBook-Pro.local>
2021-02-10 10:57:50 -0600

    manual_push

--- jenkins.py
+++ jenkins.py
@@ -43,7 +43,7 @@ class JENKINS():
 			if flag and build['building'] is False and build['result'] in ['SUCCESS', 'FAIL']:
 				# if jobStarted, jobComplete, and jobHasResult:
 				running = False
-		
+			if 'result' not in build.keys(): build['result'] = "<missing>"
 			print('  '.join([str(build['building']),build['id'],build['result']]))
 			time.sleep(1)
 			text = []
