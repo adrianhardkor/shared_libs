44049536e94ec0ec9fb19f3ff1264d2d66c30330
Adrian <adrian@Adrians-MacBook-Pro.local>
2021-02-10 18:52:59 -0600

    manual_push

--- u.py
+++ u.py
@@ -9,3 +9,6 @@ import velocity
 V = velocity.VELOCITY('10.88.48.31', 'akrygows', wc.argv_dict['p'])
 INV = V.GetInventory(); # device ipAddress
 wc.jd(INV)
+
+# /velocity/api/inventory/v13/template/{templateId}/sessions/{sessionId}?merge= <NO/INVENTORY/FULL>
+# /ito/executions/v1/agents
