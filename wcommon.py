338047d00d87f0368c9de580a41bd6832f10f272
Adrian <adrian@Adrians-MacBook-Pro.local>
2021-02-11 18:55:32 -0600

    manual_push

--- wcommon.py
+++ wcommon.py
@@ -1185,7 +1185,7 @@ def ComplianceReport(ansibleIPs, result1):
 def GetTemplatenameByModel(Model):
 	if Model == 'Red Hat': return('Server')
 	elif Model.lower() == 'mx960': return('Router')
-	else: print('wc.GetTemplatenameByModel Model not coded'); exit(5)
+	else: print('wc.GetTemplatenameByModel Model not coded: ' + Model); exit(5)
 
 wait_start()
 global current_time
