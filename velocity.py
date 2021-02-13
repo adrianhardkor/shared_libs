5932bc6584592372492349591ad3c640d520f1c8
Adrian <adrian@Adrians-MacBook-Pro.local>
2021-02-12 18:01:57 -0600

    manual_push

--- velocity.py
+++ velocity.py
@@ -404,7 +404,6 @@ class VELOCITY():
 		all_ports = self.REST_GET('/velocity/api/inventory/v13/device/%s/ports' % device['id'], params={'includeProperties':True})['ports']
 		out[device['name']]['ports'] = {}
 		PGs = self.GetDevicePGs(device['id'])
-		wc.jd(PGs)
 		for p in all_ports:
 			out,ports = self.FormatPorts(out, device, PGs, p, ports)
 			out[device['name']]['ports'][p['name']] = ports[p['name']]
