e92cc82798c277bc1d4d73f3e7b478c9d65dd085
adrianhardkor <adrian.krygowski@wowinc.com>
2021-03-15 13:55:50 -0500

    try

--- velocity.py
+++ velocity.py
@@ -384,6 +384,7 @@ class VELOCITY():
 			if self.INV[device_name]['templateName'] == 'Server': templateName = 'Server Port'
 			elif self.INV[device_name]['templateName'] == 'Modem': templateName = 'Modem Port'
 			else: templateName = 'Network Port'
+			wc.pairprint(self.INV[device_name]['templateName'], templateName)
 			args = {}
 			args['name'] = port_name
 			args['templateId'] = self.GetTemplates(templateName=templateName)['id']
