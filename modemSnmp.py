e135d389d83b4c3d57ebaa8b460088c79b0ec2aa
adrianhardkor <adrian.krygowski@wowinc.com>
2021-03-16 10:16:13 -0500

    manual

--- modemSnmp.py
+++ modemSnmp.py
@@ -41,6 +41,7 @@ class MODEMSNMP():
 		data = wc.exec2('snmpwalk -v2c -c %s -m all %s %s' % (self.community, ip, 'ipNetToMediaPhysAddress'))
 		for d in data.split('\n'):
 			d = d.split('=')
+			wc.pairprint('ipNetToMediaPhysAddress', d)
 			value =  wc.cleanLine(d[-1])
 			d = d[0].split('.')
 			snmpjunk = d.pop(0)
