8edaed5cc30f683174479e6facead6ce2dc8a86a
adrianhardkor <adrian.krygowski@wowinc.com>
2021-03-23 21:22:12 -0500

    manual

--- velocity.py
+++ velocity.py
@@ -522,8 +522,8 @@ class VELOCITY():
 			value['id'] = p['id']
 			value[p['device1']['name'] + '_' + p['port1']['name']] = 1
 			value[p['device2']['name'] + '_' + p['port2']['name']] = 2
-			if p['port1']['name'] not in out[p['device1']['name']]['ports'].keys(): wc.jd(out[p['device1']['name'])
-			if p['port2']['name'] not in out[p['device2']['name']]['ports'].keys(): wc.jd(p['device2']['name'])
+			if p['port1']['name'] not in out[p['device1']['name']]['ports'].keys(): wc.jd(out[p['device1']['name']])
+			if p['port2']['name'] not in out[p['device2']['name']]['ports'].keys(): wc.jd(p['device2']['name']])
 			out[p['device1']['name']]['ports'][p['port1']['name']]['connections'][connection_name] = value
 			out[p['device2']['name']]['ports'][p['port2']['name']]['connections'][connection_name] = value
 		return(out)
