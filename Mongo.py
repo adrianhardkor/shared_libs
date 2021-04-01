c7c75656ea4ab0fc0a1ee4abec0cf192cb7ce585
adrianhardkor <adrian.krygowski@gmail.com>
2021-04-01 12:53:40 -0400

    manual

--- Mongo.py
+++ Mongo.py
@@ -45,19 +45,30 @@ class MDB():
 			_TEMPLATE.objects.update(**criteria_SET)
 			wc.pairprint('Mongo._PUT\tALL', criteria_SET)
 			return()
-		current = self._GET(_TEMPLATE, criteria=criteria_GET); # per device
+		current = self._GET(_TEMPLATE, criteria=criteria_GET).first(); # per device
 
 		# Compare Current vs. New = rebuild criteria_SET
 		cSET = {}
-		currentJSON = json.loads(current.to_json())[0]
+		currentJSON = json.loads(current.to_json())
 		for old in criteria_SET.keys():
 			if old not in currentJSON:
 				cSET[old] = criteria_SET[old]
 			elif currentJSON[old] != criteria_SET[old]:
 				cSET[old] = criteria_SET[old]
+		# {'ports': [{'name': 'ae0'}]}
+
+		#current = _TEMPLATE.objects(_id="....").first()
+		#for port in current.ports:
+		#    if port.text == "title":
+		#        port.value = "placeholder for real update"
+		#current.save()
+
+		# _TEMPLATE.objects(_id="...", ports__name="findvalue").update(set__ports__S__value="updatevalue")
+		# doc = MyModel.objects.find(<model-key> = <model-val>, <embedded-doc-key>__<embedded-doc-lookup-key> = <lookup-key-val>)
+		# doc.update(set__<embedded-doc-key>__S__<embedded-doc-lookup-key> = <new-val>)
 		if cSET != {}:
 			current.update(**cSET)
-			# current.reload()
+			current.save(); # reload()
 			wc.pairprint('Mongo._PUT:  ' + str(criteria_GET), sorted(list(cSET.keys())))
 	def _POST(self, _TEMPLATE, criteria_SET, name):
 		_OBJECT = _TEMPLATE(**criteria_SET)
@@ -179,7 +190,7 @@ class velPort(MONGO.M.Document):
 	isLocked = MONGO.M.StringField()
 	isOnline = MONGO.M.StringField()
 	connections = MONGO.M.StringField(); # uuid? V is p2p so String
-	top_reserv = MONGO.M.ListField(MONGO.M.ReferenceField(velTopologyReservation))
+	top_reserv = MONGO.M.ListField(MONGO.M.EmbeddedDocumentField(velTopologyReservation, dbref=True))
 	pass
 
 class velDevice(MONGO.M.Document):
@@ -196,8 +207,8 @@ class velDevice(MONGO.M.Document):
 	driver = MONGO.M.StringField(); # pull from template
 	connections = MONGO.M.StringField(); # uuid? V is p2p so String
 
-	ports = MONGO.M.ListField(MONGO.M.ReferenceField(velPort)) 
-	top_reserv = MONGO.M.ListField(MONGO.M.ReferenceField(velTopologyReservation))
+	ports = MONGO.M.ListField(MONGO.M.EmbeddedDocumentField(velPort, dbref=True)) 
+	top_reserv = MONGO.M.ListField(MONGO.M.EmbeddedDocumentField(velTopologyReservation, dbref=True))
 	pass
 
 # ansible host/group vars?
@@ -232,10 +243,10 @@ class Router(MONGO.M.Document):
 	ansible_host_vars = MONGO.M.DictField()
 	ansible_ready = MONGO.M.DictField()
 	# ansible host/group vars?
-	ports = MONGO.M.ListField(MONGO.M.ReferenceField(Port))	
-	velocityARC = MONGO.M.ReferenceField(velDevice)
+	ports = MONGO.M.ListField(MONGO.M.EmbeddedDocumentField(Port, dbref=True))
+	velocityARC = MONGO.M.EmbeddedDocumentField(velDevice, dbref=True)
 	IPAM = MONGO.M.DictField(); # {'10.88.48.0/23':fxp0}
-	# NCS = MONGO.M.ReferenceField(mNCS); # rack-loc?
+	# NCS = MONGO.M.EmbeddedDocumentField(mNCS, dbref=True); # rack-loc?
 	timestamp = MONGO.M.StringField()
 	pass
 
