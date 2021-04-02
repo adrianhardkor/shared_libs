663351e9810334fe797f5e1fdac23bf6c4ba04dd
adrianhardkor <adrian.krygowski@gmail.com>
2021-04-02 14:42:09 -0400

    manual

--- Mongo.py
+++ Mongo.py
@@ -318,10 +318,10 @@ class CMTS(MONGO.M.Document):
 	timestamp = MONGO.M.StringField()
 	pass
 
-MONGO._DELETE(Router, criteria={}, force=True)
-MONGO._DELETE(CMTS, criteria={}, force=True)
-MONGO._DELETE(Server, criteria={}, force=True)
-MONGO._DELETE(SG, criteria={}, force=True)
+# MONGO._DELETE(Router, criteria={}, force=True)
+# MONGO._DELETE(CMTS, criteria={}, force=True)
+# MONGO._DELETE(Server, criteria={}, force=True)
+# MONGO._DELETE(SG, criteria={}, force=True)
 try:
 	MONGO._DELETE(Modem, criteria={}, force=True)
 except Exception:
