39d8efae0447cf9c253ab05e272dc12b5b9a6455
adrianhardkor <adrian.krygowski@gmail.com>
2021-04-16 13:20:47 -0400

    manual

--- loggerJenkins.py
+++ loggerJenkins.py
@@ -5,9 +5,9 @@ sys.path.insert(1, './lib/shared_libs/')
 import wcommon as wc
 import mhandle
 
-# ./lib/shared_libs/loggerJenkins.py flask=IP:PORT  BUILD_TAG=${env.BUILD_TAG}  data=${data}
+# ./lib/shared_libs/loggerJenkins.py BUILD_TAG=${env.BUILD_TAG}  data=${data}
 
-MH = mHANDLE(flaskIP=wc.argv_dict['flask'].split(':')[0], flaskPort=wc.argv_dict['flask'].split(':')[1])
+MH = mHANDLE(flaskIP=wc.env_dict['FLASK_IP'], flaskPort=wc.env_dict['FLASK_PORT'])
 MH.who = 'JENKINS'
 MH.runId = wc.argv_dict['BUILD_TAG']
 MH._LOGGER(wc.argv_dict['data']); # creates timestamps too
