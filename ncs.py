b39831d429f73edf44fafaad81c7613faec3df24
adrianhardkor <adrian.krygowski@gmail.com>
2021-05-03 17:35:54 -0400

    manual

--- ncs.py
+++ ncs.py
@@ -7,6 +7,7 @@ import hmac
 import hashlib
 
 
+
 class NCS():
 	def __init__(self, host, key, secret_hmac):
 		self.host = host
@@ -19,6 +20,14 @@ class NCS():
 		message = bytes(message, 'UTF-8')
 		digester = hmac.new(key, message, hashlib.sha1)
 		return(digester.hexdigest())
+	def Format(self, data):
+		out = {}
+		for d in data['results']:
+			ID = d['fields']['id']['value']
+			if ID not in out.keys(): out[ID] = {}
+			for f in d['fields'].keys():
+				out[ID][f] = d['fields'][f]['value']
+		return(out)
 	def REST_POST(self, endpoint, args={}):
 		s = Session()
 		base_api_url = self.url + endpoint
@@ -29,12 +38,15 @@ class NCS():
 		wc.jd(self.headers)
 		data = s.post(self.url + endpoint, headers=self.headers, json=json_payload)
 		return(data.json())
+	def LoadTables(self):
+		multiplexer_manufacturers = self.REST_POST('/multiplexer_manufacturers/list')
+		multiplexer_manufacturers = self.format(multiplexer_manufacturers)
+		wc.jd(multiplexer_manufacturers)
 	def GetInventory(self, site_location_id):
 		data = self.REST_POST('/multiplexers/list', args={'criteria':{'site_location_id':{'operator':'=','data':str(site_location_id)}}})
+		data = self.Format(data)
 		return(data)
 
-key = '3b4fc64446838561fdef78d18f6e963f'
-myhmac = 'b4814b0cfbd19f032d3c61e282cbfa6f'
 N = NCS('wow-sandbox.n-c-s.net', key=key, secret_hmac=myhmac)
 wc.jd(N.GetInventory('61325'))
 
