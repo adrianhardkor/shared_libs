1cb4a88b347e19e7fd300c0493e59ce8c92703fc
adrianhardkor <adrian.krygowski@wowinc.com>
2021-06-09 15:45:24 -0400

    manual

--- wcommon.py
+++ wcommon.py
@@ -1234,7 +1234,7 @@ def getFnameScaffolding(fname_list):
 	for f in fname_list:
 		lf = f.lower()
 		sf = mcsplit(lf, ['.','/'])
-		if 'yml' in sf and ('dcim' in sf or 'itsm' in sf or 'cable' in sf) and len(f.split('-') >= 5:
+		if 'yml' in sf and ('dcim' in sf or 'itsm' in sf or 'cable' in sf) and len(f.split('-')) >= 5:
 			if sf[2] not in result.keys(): result[sf[2]] = {'dcim':None,'itsm':None,'cable':None}
 			result[sf[2]][sf[3]] = read_yaml(sf[3])
 	return(result)
